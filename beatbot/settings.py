245c24bf664cb46d03ccade5d425b695e19192a4
steffen <slyngel@gmail.com>
2019-08-06 20:49:12 +0200

    static ...

--- beatbot/settings.py
+++ beatbot/settings.py
@@ -58,6 +58,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+MIDDLEWARE_CLASSES = (
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+)
+
 ROOT_URLCONF = 'beatbot.urls'
 
 TEMPLATES = [
